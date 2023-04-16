4263f19e520a06a9b7a98234b166015da49d3cbf
Himanshu <himbaghel19@gmail.com>
2023-04-16 20:10:59 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+import Slideshow from './Slideshow'
+import { Box, Container, Typography } from '@mui/material'
+import italy from './Images/Italy.jpg'
+import maldives from './Images/Maldives.jpg'
+import tajMahal from './Images/TajMahal.jpg'
+import dubai from './Images/Dhubai.jpg'
+import england from './Images/England.jpg'
+import goldenTemple from './Images/GoldenTemple.jpg'
+import paris from './Images/Paris.jpg'
+import russia from './Images/Russia.jpg'
+const App = () => {
+
+  const images = [
+    tajMahal,
+    italy,
+    maldives,
+    dubai,
+    england,
+    paris,
+    russia,
+    goldenTemple
+  ]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container maxWidth={false} sx={{display:'flex',flexWrap:'wrap'}}>
+      <style>
+        @import url('https://fonts.googleapis.com/css2?family=Playfair+Display:wght@600&display=swap');
+      </style>
+      <Box height='100%' sx={{
+        display: 'flex',
+        justifyContent: 'space-evenly',
+        paddingTop: '3%',
+        flexWrap: { xs: 'wrap-reverse', md: 'nowrap' }
+      }}>
+
+        <Slideshow images={images} />
+      </Box>
+     
+    </Container>
+
+  )
 }
 
-export default App;
+export default App
